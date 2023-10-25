5940fc66fa72d32532d7de1c9329a396c4e6af75
Rennan Bacili <rennanbaccili@gmail.com>
2023-10-25 10:46:47 -0300

    correção de bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:`<a [routerLink]="['admin']">link</a><br>
-  <a [routerLink]="['sign']">sign</a>
-  <router-outlet></router-outlet>`,
+  template:`<router-outlet></router-outlet>`,
 })
 export class AppComponent {
   title = 'curso-de-angular-auth-front';
